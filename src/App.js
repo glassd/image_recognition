ef528746ed57e0c50844e4b0014fa448b8f21a4c
David Glass <glassd@glassd.ca>
2021-07-13 15:13:26 -0400

    finishing website layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
-import Navigation from './components/Navigation'
-import Logo from './components/Logo'
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const particlesParams = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles params={particlesParams} className='particles' />
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm />
-      <ImageRegonition /> */}
+      <Rank />
+      <ImageLinkForm />
+      {/* <ImageRegonition /> */}
+
     </div>
   );
 }
